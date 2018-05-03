31c1f6e8c2f47812747004f8689f5f3913f3fab3
Riya Abraham <riyamaryabraham12@gmail.com>
2018-05-02 20:53:16 -0500

    modifications2

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -7,7 +7,6 @@ var config = {
     messagingSenderId: "915753872712"
 };
 firebase.initializeApp(config);
-
 // Create a variable to reference the database.
 var database = firebase.database();
 //initialize
@@ -15,10 +14,6 @@ var train_name = '';
 var destination = '';
 var time = 0;
 var frequency = 0;
-/******************/
-
-/***********************/
-
 // Capture Button Click
 $("#btn").on("click", function (event) {
     event.preventDefault();
@@ -28,108 +23,62 @@ $("#btn").on("click", function (event) {
     destination = $("#destination").val().trim();
     var time = $("#time").val();
     var frequency = $("#frequency").val();
-    /**************************/
-    
-
     //this will prevent page from submitting an empty form
-
-    if ($.trim($("#destination").val()) === "" || $.trim($("#train_name").val()) === "" || $.trim($("#time").val()) === ""|| $.trim($("#frequency").val()) === "") {
+    if ($.trim($("#destination").val()) === "" || $.trim($("#train_name").val()) === "" || $.trim($("#time").val()) === "" || $.trim($("#frequency").val()) === "") {
         alert('You did not fill out one or more of the fields');
         return false;
     }
-    
     database.ref().push({
         Train_name: train_name,
         Destination: destination,
         Time: time,
         frequency: frequency,
         dateAdded: firebase.database.ServerValue.TIMESTAMP
-    
     });
-    
 });
-/*********************************/
-
-
 database.ref().orderByChild("dateAdded").on("child_added", function (snapshot) {
     var sv = snapshot.val();
-
     ///math
     console.log(sv.frequency);
     console.log(sv.Time);
- // First Time (pushed back 1 year to make sure it comes before current time)
+    // First Time (pushed back 1 year to make sure it comes before current time)
     var firstTimeConverted = moment(sv.Time, "hh:mm").subtract(1, "years");
     console.log(firstTimeConverted);
-
     // Current Time
     var currentTime = moment();
     console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-
     // Difference between the times
     var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
     console.log("DIFFERENCE IN TIME: " + diffTime);
-
     // Time apart (remainder)
     var tRemainder = diffTime % sv.frequency;
     console.log(tRemainder);
-
     // Minute Until Train
     var tMinutesTillTrain = sv.frequency - tRemainder;
     console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-
     // Next Train
     var nextTrain = moment().add(tMinutesTillTrain, "minutes");
     console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
-  var row = $("<tr>");
-  row.attr('id',"id");
+    var row = $("<tr>");
+    row.attr('id', "id");
     row.append($("<td>").text(sv.Train_name));
     row.append($("<td>").text(sv.Destination));
     row.append($("<td>").text(sv.frequency));
     row.append($("<td>").text(moment(nextTrain).format('LT')));
     row.append($("<td>").text(tMinutesTillTrain));
-
-    var update = $("<button>");
-    update.attr('id',"update");
-    
-    row.append(update.text("UPDATE"));
-    row.append($("<button>").attr('id',"remove").text("REMOVE"));
-    
     $("#table").append(row);
-
-   
-//this step clears the text field after inputting to the screen and databse
+    //this step clears the text field after inputting to the screen and databse
     $("#train_name").val('');
     $("#destination").val('');
     $("#time").val('');
     $("#frequency").val('');
-
-    /***************************/
-  
-    /******************* */
 }, function (errorObject) {
     console.log("Errors handled: " + errorObject.code);
 });
 
-var rootRef = firebase.database().ref().child("traindetails");
-$("#table").on('click','#remove', function(e){
-    var $row = $(this).closest('tr'),
-       rowId = $row.data('id');
-    var assetKey = rootRef.child("id");
-    //it should remove the firebase object in here
-    rootRef.child(assetKey).remove()
-    //after firebase confirmation, remove table row
-    .then(function() {
-      $row.remove();
-    })
-    //Catch errors
-    .catch(function(error) {
-      console.log('ERROR');
-    });  
-});
 
 
 
-  
 
 
 
