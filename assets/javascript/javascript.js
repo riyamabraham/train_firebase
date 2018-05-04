58fa628c53b384baedb57226554a1000c22115b1
Riya Abraham <riyamaryabraham12@gmail.com>
2018-05-04 11:16:21 -0500

    tables

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -67,6 +67,8 @@ database.ref().orderByChild("dateAdded").on("child_added", function (snapshot) {
     row.append($("<td>").text(moment(nextTrain).format('LT')));
     row.append($("<td>").text(tMinutesTillTrain));
     $("#table").append(row);
+    
+
     //this step clears the text field after inputting to the screen and databse
     $("#train_name").val('');
     $("#destination").val('');
